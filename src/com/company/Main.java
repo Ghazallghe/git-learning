78f15461ec7baece21cb800ab7ac4434e481fd6f
Ghazal <ghazaal.laghaee@gmail.com>
2021-03-11 02:26:50 +0330

    "name added"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main{
     public static void main(String [] args){
         System.out.println("Hello git");
-        System.out.println("My name is: <your name>");
+        System.out.println("My name is: Ghazal");
         System.out.println("My student ID is <your id>");
     }
 }
